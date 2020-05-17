c2c95f97d19992d89bc1492a8d5ab0f194e4bbc6
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-05-17 16:21:35 -0600

    Create pasajero.cs & pasajero.cs.meta

---
+++ Assets/Code/Pasajero.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pasajero : MonoBehaviour
+{
+    private string Nombre { get; set; }
+    private string Nacionaliad { get; set; }
+    private string Viaja_a { get; set; }
+    private int Estancia { get; set; }
+    private string[] Sintomas { get; set; }
+    private float Temperatura { get; set; }
+
+    public Pasajero()
+    {
+    }
+
+}
