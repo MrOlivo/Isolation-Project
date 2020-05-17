9b4800f90569aeac264487e63e42f731df168a2b
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-05-17 15:37:09 -0600

    Create Controlador

---
+++ Assets/Code/Controlador.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Controlador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            CambiarEscena("StartMenu");
+            print("Tecla Escape presionada");
+        }
+    }
+
+    public void CambiarEscena(string nombre)
+    {
+        print("Cambiando a la Escena " + nombre);
+        SceneManager.LoadScene(nombre);
+    }
+
+    public void CambiarTexto(Text txtElement)
+    {
+        txtElement.text = "Hola Unity";
+    }
+
+    public void Salir()
+    {
+        print("Saliendo del Juego");
+        Application.Quit();
+    }
+}
