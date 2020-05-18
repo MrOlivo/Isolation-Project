b299d07568688f3812f2fcbe059b12dc6dd1a261
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-05-17 18:44:59 -0600

    Update Controlador.cs

--- Assets/Code/Controlador.cs
+++ Assets/Code/Controlador.cs
@@ -6,17 +6,21 @@ using UnityEngine.UI;
 
 public class Controlador : MonoBehaviour
 {
-    private Text estancia;
-    private Text habitos;
-    private Text sintomas;
-    private Text temperatura;
-    private Text segundos_restantes;
-    private Text pasajeros_restantes;
+    public Text estancia;
+    public Text habitos;
+    public Text sintomas;
+    public Text temperatura;
+    public Text segundos_restantes;
+    public Text pasajeros_restantes;
+    public Text nombre;
+    public Text nacionalidad;
+    public Text viaja_a;
+    public Dictionary<int, string[]> listaPasajeros;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        CargarDatos();
     }
 
     // Update is called once per frame
@@ -45,14 +49,24 @@ public class Controlador : MonoBehaviour
         txtElement.text = "Hola Unity";
     }
 
+
+    public void CargarDatos()
+    {
+        Pasajero pasajero = new Pasajero();
+        listaPasajeros = pasajero.GetListaPasajeros();
+        int pass = 0;
+        nombre.text = listaPasajeros[pass][0];
+        nacionalidad.text = listaPasajeros[pass][1];
+        viaja_a.text = listaPasajeros[pass][2];
+        estancia.text = listaPasajeros[pass][3];
+        habitos.text = listaPasajeros[pass][4];
+        sintomas.text = listaPasajeros[pass][5];
+        temperatura.text = listaPasajeros[pass][6];
+    }
+
     public void Salir()
     {
         print("Saliendo del Juego");
         Application.Quit();
-    }
-
-    public void CargarDatos()
-    {
-
     }
 }
