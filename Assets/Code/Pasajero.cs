a47cc17bacd03f948bd0823585bd7f34b5587ea3
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-05-17 18:45:05 -0600

    Update Pasajero.cs

--- Assets/Code/Pasajero.cs
+++ Assets/Code/Pasajero.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Pasajero : MonoBehaviour
 {
-    private string Nombre { get; set; }
-    private string Nacionaliad { get; set; }
-    private string Viaja_a { get; set; }
-    private int Estancia { get; set; }
-    private string[] Sintomas { get; set; }
-    private float Temperatura { get; set; }
+    private Dictionary<int, string[]> listaPasajeros;
+
+    public Dictionary<int, string[]> GetListaPasajeros()
+    {
+        return listaPasajeros;
+    }
+
+    public void SetListaPasajeros(Dictionary<int, string[]> value)
+    {
+        listaPasajeros = value;
+    }
+
+    private string[] informacion;
 
     public Pasajero()
     {
+        SetListaPasajeros(new Dictionary<int, string[]>());
+        informacion = new string[] {"Nombre", "Nacionalidad", "Viaja a",
+            "Estancia", "Habitos", "Sintomas", "Temperatura", "Positivo" };
+
+        GetListaPasajeros().Add(0, informacion);
+        GetListaPasajeros().Add(1, informacion);
+        GetListaPasajeros().Add(2, informacion);
     }
 
 }
