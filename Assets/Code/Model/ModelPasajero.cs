fa070aec5c49bbea2f52765f996d7c650bf4e069
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-05-19 01:15:33 -0600

    Update ModelPasajero.cs

--- Assets/Code/Model/ModelPasajero.cs
+++ Assets/Code/Model/ModelPasajero.cs
@@ -3,19 +3,20 @@ public class ModelPasajero
 {
     public string Nombre { get; set; }
     public string Nacionalidad { get; set; }
-    public string Viaja_a { get; set; }
+    public string Destino { get; set; }
     public string Estancia { get; set; }
     public string Habitos { get; set; }
     public string Sintomas { get; set; }
     public int Temperatura { get; set; }
     public bool Covid { get; set; }
 
-    public ModelPasajero(string nombre, string nacionalidad, string viaja_a, string estancia, string habitos, string sintomas, int temperatura, bool covid)
+    public ModelPasajero(string nombre, string nacionalidad, string destino, string estancia, string habitos, string sintomas, int temperatura, bool covid)
     {
         Nombre = nombre;
         Nacionalidad = nacionalidad;
-        Viaja_a = viaja_a;
+        Destino = destino;
         Estancia = estancia;
+        //  Eliminar los habitos
         Habitos = habitos;
         Sintomas = sintomas;
         Temperatura = temperatura;
