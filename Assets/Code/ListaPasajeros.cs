8b47ec6ec3de3132663dd7d46800f411fdda24ea
Edwin <kraken>
2020-05-25 09:52:09 -0600

    Update ListaPasajeros.cs

--- Assets/Code/ListaPasajeros.cs
+++ Assets/Code/ListaPasajeros.cs
@@ -12,16 +12,16 @@ public class Pasajero : MonoBehaviour
 
         ModelPasajero informacion;
 
-        informacion = new ModelPasajero("A", "G", "UAIS", 9, "Comer carne cruda", "Ninguno", 36, false);
+        informacion = new ModelPasajero("Alondra", "Guatemala", "Honduras", 9, "Comer carne cruda", "Ninguno", 36, false);
         Pasajeros.Add(0, informacion);
 
-        informacion = new ModelPasajero("B", "H", "VAIS", 5, "Puede volar", "Ninguno", 36, true);
+        informacion = new ModelPasajero("Briseida", "Holanda", "Venezuela", 5, "Puede volar", "Ninguno", 36, true);
         Pasajeros.Add(1, informacion);
 
-        informacion = new ModelPasajero("C", "I", "CAIS", 5, "Puede volar", "Ninguno", 36, true);
+        informacion = new ModelPasajero("Carlos", "Venezuela", "Guatemala", 5, "Puede volar", "Ninguno", 36, true);
         Pasajeros.Add(2, informacion);
 
-        informacion = new ModelPasajero("D", "K", "CAIS", 5, "Puede volar", "Ninguno", 36, true);
+        informacion = new ModelPasajero("Diego", "Honduras", "Guatemala", 5, "Puede volar", "Ninguno", 36, true);
         Pasajeros.Add(3, informacion);
     }
 
