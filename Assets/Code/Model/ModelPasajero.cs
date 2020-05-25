6f5a119a59f8a0300eba26acd85f46a298baf942
Edwin <kraken>
2020-05-25 11:19:45 -0600

    Update ModelPasajero.cs

--- Assets/Code/Model/ModelPasajero.cs
+++ Assets/Code/Model/ModelPasajero.cs
@@ -3,17 +3,24 @@
 [Serializable]
 public class ModelPasajero
 {
+    public int Id { get; set; }
     public string Nombre { get; set; }
     public string Nacionalidad { get; set; }
     public string Destino { get; set; }
     public int Estancia { get; set; }
     public string Habitos { get; set; }
     public string Sintomas { get; set; }
-    public int Temperatura { get; set; }
+    public float Temperatura { get; set; }
     public bool Covid { get; set; }
 
-    public ModelPasajero(string nombre, string nacionalidad, string destino, int estancia, string habitos, string sintomas, int temperatura, bool covid)
+    public ModelPasajero()
     {
+
+    }
+
+    public ModelPasajero(int id, string nombre, string nacionalidad, string destino, int estancia, string habitos, string sintomas, float temperatura, bool covid)
+    {
+        Id = id;
         Nombre = nombre;
         Nacionalidad = nacionalidad;
         Destino = destino;
