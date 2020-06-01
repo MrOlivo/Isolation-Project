9e425c6a2926cb17036c4377fb419761d6bde5e8
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-06-01 10:06:53 -0600

    Update ListaPasajeros.cs

--- Assets/Code/ListaPasajeros.cs
+++ Assets/Code/ListaPasajeros.cs
@@ -9,8 +9,10 @@ public class Pasajero : MonoBehaviour
     public Pasajero()
     {
         Pasajeros = new Dictionary<int, ModelPasajero>();
+        string file = "MOCK_DATA";
+        string path = "Assets/Code/"+ file +".csv";
 
-        var objs = Sinbad.CsvUtil.LoadObjects<ModelPasajero>("Assets/Code/MOCK_DATA.csv");
+        var objs = Sinbad.CsvUtil.LoadObjects<ModelPasajero>(path);
 
         for(int i = 0; i < objs.Count; i++){
             Pasajeros.Add(i, objs[i]);
