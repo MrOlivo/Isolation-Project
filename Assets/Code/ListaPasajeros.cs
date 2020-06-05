91d50110ca17c0a758cb1afa1f27525582aeb8ab
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-06-04 19:37:45 -0600

    Update ListaPasajeros.cs
    Random positive generator xD

--- Assets/Code/ListaPasajeros.cs
+++ Assets/Code/ListaPasajeros.cs
@@ -9,13 +9,19 @@ public class Pasajero : MonoBehaviour
     public Pasajero()
     {
         Pasajeros = new Dictionary<int, ModelPasajero>();
+        System.Random rn = new System.Random();
+
         string file = "MOCK_DATA";
         string path = "Assets/Code/"+ file +".csv";
 
         var objs = Sinbad.CsvUtil.LoadObjects<ModelPasajero>(path);
 
         for(int i = 0; i < objs.Count; i++){
+
+            objs[i].Covid = (objs[i].Temperatura > 38 && rn.Next(0, 9) > 7) ? true : false;
+
             Pasajeros.Add(i, objs[i]);
+
         }
     }
 
