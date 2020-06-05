dfac0220448ec3ecf31a86ff49c7ff20a53004ae
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-06-05 13:38:41 -0600

    Create Dias.cs

---
+++ Assets/Code/Dias.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Dias : MonoBehaviour
+{
+    TMPro.TextMeshProUGUI txtElement;
+
+    private static int dias = 1;
+    private float timeLeft = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtElement.text = "Dia " + dias;
+        dias++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft > 0.0)
+        {
+            CammbiarEscena();
+        }
+    }
+
+    public void CammbiarEscena()
+    {
+        SceneManager.LoadScene("Principal");
+    }
+}
